15343d848b26d055331dc21eb1813ff067baccab
6ix78 <87336515+6ix78@users.noreply.github.com>
2025-04-29 07:06:16 +0100

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 const Button = ({ styles }) => (
- <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-   <span>
-     <a href="https://daring-turtle-33.accounts.dev/sign-up?redirect_url=https%3A%2F%2Fclerk-authentication-starter-ivory.vercel.app%2F%3F__dev_session%3Ddvb_2wO9F3Q1OXw0GvpnDN9JScZ5asb%26__clerk_db_jwt%3Ddvb_2wO9F3Q1OXw0GvpnDN9JScZ5asb%23__clerk_db_jwt%5Bdvb_2wO9F3Q1OXw0GvpnDN9JScZ5asb%5D">
+ <button href="https://zws.im/󠁨󠁥󠁺󠁰󠁵󠁺󠁷" type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
        Get Started
- </span></a> </button>
+ </button>
 );
 
 export default Button;
